ec27395aad6969fea4040dae8fa299d1b86edcf4
Kaxxu <pawmentel@gmail.com>
2022-06-07 15:07:51 +0200

    fix: reduce terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -15,25 +15,6 @@
 #  }
 #}
 
-# TERRAFORM S3
-#terraform {
-#  required_version = "= 1.2.1"
-#
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "= 4.6.0"
-#    }
-#  }
-#
-#  backend "s3" {
-#    encrypt = true
-#    bucket  = "terraform-state-kaxxu"
-#    key     = "templar/templar.tfstate"
-#    region = "eu-west-1"
-#  }
-#}
-
 terraform {
   required_version = "= 1.2.1"
 
@@ -45,8 +26,7 @@ terraform {
   }
 
   backend "s3" {
-    # configuration see: params/*.backend
-    #
+    # Configuration see: params/*.backend
     # Commands:
     # terraform init -backend-config=params/templar.backend -reconfigure
     # terraform plan -var-file=params/templar.tfvars
